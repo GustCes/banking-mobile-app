207e0b86d3f692fcbbc0c5916221ce47995b22d0
proyecto <criolloc449@gmail.com>
2025-11-19 16:22:14 -0500

    un poco de avance

---
+++ app/src/screens/Splash.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import {
+    Image,
+    StyleSheet,
+    View
+} from 'react-native';
+
+const Splash = ({ onFinish }: {onFinish:() => void } ) => {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            onFinish();
+        }, 5000);
+        return () => clearTimeout(timer);
+    }, [onFinish]);
+
+    return (
+        <View style = {styles.container}>
+            <Image
+                source={require('../assets/images/shrek.png')}
+                style={styles.logo}
+                resizeMode='contain'
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#a0ff8dff',
+    },
+    logo: {
+        width: 200,
+        height: 200,
+    },
+});
+
+export default Splash;
